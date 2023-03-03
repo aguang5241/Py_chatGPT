2c26953e2bf65bbdd9dfda85321a84b2b3be00cf
aguang5241 <gliu4@wpi.edu>
2023-03-03 11:28:38 -0500

    modified:   Py_chatGPT.py

--- Py_chatGPT.py
+++ Py_chatGPT.py
@@ -2,17 +2,29 @@ import openai
 import os, time
 
 # Get current time and only keep the numbers
-now = time.strftime("%Y%m%d%H%M%S", time.localtime())[2:]
+now = time.strftime('%H%M%S', time.localtime())
+
+# To choose if you want to continue a previous chat or start a new one
+while True:
+    choice = input('Enter the name of the file you want to continue or enter "new" to start a new chat: ')
+    if choice == 'new':
+        chatlog = f'chatlog_{now}.txt'
+        break
+    elif os.path.isfile(choice):
+        chatlog = choice
+        break
+    else:
+        print('File does not exist. Please try again.')
 
 openai.api_key_path = os.path.join(os.path.dirname(__file__), 'openai_api_key.txt')
 
 while True:
     user = input('User: ')
-    with open(f'chatlog_{now}.txt', 'a') as f:
+    with open(chatlog, 'a') as f:
         f.write('user: ' + user + '\n')
 
     messages = []
-    with open(f'chatlog_{now}.txt', 'r') as f:
+    with open(chatlog, 'r') as f:
         content = f.readlines()
         content = [x.strip() for x in content]
 
@@ -24,7 +36,7 @@ while True:
 
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
     assistant = completion['choices'][0]['message']['content']
-    with open(f'chatlog_{now}.txt', 'a') as f:
+    with open(chatlog, 'a') as f:
         f.write('assistant: ' + assistant + '\n')
 
     # print assistant response in blue
